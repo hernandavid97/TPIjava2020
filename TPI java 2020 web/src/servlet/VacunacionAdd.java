be230a6814046afc3771a29d903436a032a8e0ed
Hernán Arcari <hernan.arcari@gmail.com>
2024-03-28 18:17:14 -0300

    Agregado alta vacunacion

---
+++ TPI java 2020 web/src/servlet/VacunacionAdd.java
@@ -0,0 +1,81 @@
+package servlet;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import data.DataVacunacion;
+import entities.Mascota;
+import entities.Transito;
+import entities.Usuario;
+import entities.Vacunacion;
+import logic.TransitoLogic;
+import logic.VacunacionLogic;
+
+/**
+ * Servlet implementation class TransitoAdd
+ */
+@WebServlet({ "/vacunacionadd", "/VacunacionAdd" })
+public class VacunacionAdd extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public VacunacionAdd() {
+		super();
+		DataVacunacion dv = new DataVacunacion();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("WEB-INF/VacunacionAdd.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+        String fechaString = request.getParameter("fecha_vacunacion");
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
+        Date fechaDate = null;
+        
+        try {
+	        fechaDate = formatoFecha.parse(fechaString);
+	        System.out.println(fechaDate);
+
+			Vacunacion v = new Vacunacion();
+			v.setId_vacuna(Integer.parseInt(request.getParameter("selectVacuna")));
+			v.setId_mascota(Integer.parseInt(request.getParameter("selectMascota")));
+			v.setFecha_vacunacion(fechaDate);			
+	
+			VacunacionLogic ctrlVac = new VacunacionLogic();
+			String res = ctrlVac.insertOne(v);
+			System.out.println(res);
+			request.setAttribute("message", res);
+			request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("error", "Ha ocurrido un error" );
+			request.getRequestDispatcher("/WEB-INF/VacunacionAdd.jsp").forward(request, response);
+		}
+
+	}
+}
